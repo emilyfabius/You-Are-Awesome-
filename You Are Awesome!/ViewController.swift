16326677d801e6944477b938824a6e2343858fb1
emilyfabius <fabius@bc.edu>
2021-02-07 14:43:46 -0500

    Cycling through image0 - image9

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var awesomeLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         awesomeLabel.text = ""
@@ -18,7 +20,34 @@ class ViewController: UIViewController {
 
 
     @IBAction func messageButtonPress(_ sender: UIButton) {
-        awesomeLabel.text = "You are awesome!"
-        imageView.image = UIImage(named: "image0")
+        
+        print(imageNum)
+        //let imageName = "image" + String(imageNum)
+        let imageName = "image\(imageNum)"
+        imageView.image = UIImage(named: imageName)
+        if imageNum <= 8 {
+            imageNum = imageNum + 1
+        }
+        else {
+            imageNum = 0
+        }
+        
+        
+        
+        //        let awesomeMessage = "You are awesome!"
+        //        let greatMessage = "You are great!"
+        //        let catMessage = "You are a cool cat!"
+        //
+        //        if awesomeLabel.text == awesomeMessage{
+        //            awesomeLabel.text = greatMessage
+        //            imageView.image = UIImage(named: "image1")
+        //        } else if awesomeLabel.text == greatMessage {
+        //            awesomeLabel.text = catMessage
+        //            imageView.image = UIImage(named: "image2")
+        //        } else {
+        //            awesomeLabel.text = awesomeMessage
+        //            imageView.image = UIImage(named: "image0")
+        //        }
+        
     }
 }
